0ecbd4cdc2812307a82ff56052953f9ccd53824c
Patrick Bellasi <derkling@gmail.com>
2012-01-26 19:09:11 +0100

    [PP_LINUX] Cosmetics
    Improve code sections identification.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- bbque/pp/linux.cc
+++ bbque/pp/linux.cc
@@ -88,6 +88,10 @@ LinuxPP::~LinuxPP() {
 
 }
 
+/*******************************************************************************
+ *    Platform Resources Parsing and Loading
+ ******************************************************************************/
+
 LinuxPP::ExitCode_t
 LinuxPP::RegisterClusterCPUs(RLinuxBindingsPtr_t prlb) {
 	br::ResourceAccounter &ra(br::ResourceAccounter::GetInstance());
@@ -306,6 +310,9 @@ LinuxPP::_LoadPlatformData() {
 	return pp_result;
 }
 
+/*******************************************************************************
+ *    Resources Mappign and Assigment to Applications
+ ******************************************************************************/
 
 LinuxPP::RLinuxType_t
 LinuxPP::GetRLinuxType(br::ResourcePtr_t pres) {
