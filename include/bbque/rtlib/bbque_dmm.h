b04d272faa22a3f01439e3c6c93077177c6306d0
Patrick Bellasi <derkling@gmail.com>
2012-10-25 10:21:43 +0200

    [RTLIB_DMM] Add first implementation
    This is the first implementation of the interface layer toward the Dynamic
    Memory Management Library (DMMLib) by ICCS:
    https://bitbucket.org/joko/dmmlib
    
    This integration is based on a set of symbols which are expected to be
    exported by the library:
    - dmm_knobs: is an array of knobs parameters for a run-time configuration;
    - dmm_knobs_count: the number of run-time configurations
    - dmm_rtm_init: initialize DMMLib for run-time management
    - dmm_set_knobs: tune DMMLib according to the specified set of knob parameters;
    
    These symbols are "weak", thus if the library should not export them the
    integration layer is disabled.
    
    The set of knobs parameters, entries of the dmm_knobs array, are application
    specific and could be generated using the bbque-oplb tool.
    The Operating-Point List Builder is a parser of XML files produced for example
    by the MOST Design Space Exploration tool. An example of such a file is provided
    in tools/inputTest_bbqueOpListBuilder.xml.
    All the "parameters" and "system_metrics" which names starts with "dmm." are
    used to populate the dmm_knobs array.
    NOTE: the ordinal position of parsed parameters is expected to match the
    layout of the "struct dmm_knobs_t", which is defined by the DMMLib in the
    provided <dmm/knobs.h>.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

---
+++ include/bbque/rtlib/bbque_dmm.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2012  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BBQUE_DMM_H_
+#define BBQUE_DMM_H_
+
+#include "bbque/rtlib.h"
+#include "bbque/config.h"
+#include "bbque/utils/utility.h"
+
+struct rtm_knobs_t;
+
+namespace bbque { namespace rtlib {
+
+/**
+ * @brief The class implementing the RTLib plain API
+ * @ingroup rtlib_sec03_plain
+ *
+ * This RPC mechanism is channel agnostic and defines a set of procedure that
+ * applications could call to send requests to the Barbeque RTRM.  The actual
+ * implementation of the communication channel is provided by class derived by
+ * this one. This class provides also a factory method which allows to obtain
+ * an instance to the concrete communication channel that can be selected at
+ * compile time.
+ */
+class LibDMM {
+
+public:
+
+/******************************************************************************
+ * DMM Library Configuration Interface
+ ******************************************************************************/
+
+	/**
+	 * @brief Initilize the DMM library for Run-Time management
+	 *
+	 * @param conf A reference to the DMM knob parameters table
+	 * @param count The number of different knobs configurations
+	 *
+	 * @return RTLIB_OK on success, RTLIB_ERROR otherwise
+	 */
+	static RTLIB_ExitCode_t Init(const struct rtm_knobs_t *conf, const uint32_t count);
+
+	/**
+	 * @brief Set the DMM Library with the specified knob parameters
+	 *
+	 * @param index The ID of the knob parameters into the knob
+	 * parameters table
+	 *
+	 * @return RTLIB_OK on success, RTLIB_ERROR otherwise
+	 */
+	static RTLIB_ExitCode_t SetKnobs(uint32_t index);
+
+private:
+
+	/**
+	 * @brief The LibDMM is a singleton class
+	 */
+	LibDMM();
+	~LibDMM(void);
+
+	/**
+	 * @brief True if the DMM Library has been correctly initialized for
+	 * Run-Time Management
+	 */
+	static bool initialized;
+
+};
+
+} // namespace rtlib
+
+} // namespace bbque
+
+#endif /* end of include guard: BBQUE_DMM_H_ */
---
+++ rtlib/bbque_dmm.cc
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2012  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "bbque/config.h"
+#include "bbque/rtlib/bbque_dmm.h"
+
+#include <dmmlib/knobs.h>
+
+#include <cstdio>
+#include <sys/stat.h>
+
+// Setup logging
+#undef  BBQUE_LOG_MODULE
+#define BBQUE_LOG_MODULE "dmm"
+
+/**
+ * @brief The DMM Library Knobs tuning parameters
+ *
+ * Each AWM could be assigned a different set of DMM tuning parameters.
+ * The value of such parameters is idenfitied at design time and represented
+ * into a proper rtm_knobs_t data structure.
+ * The collection of tuning values for each AWM is represente by this symbol
+ * which an application is expected to export.
+ *
+ */
+extern struct rtm_knobs_t dmm_knobs[] __attribute__((weak));
+
+
+/**
+ * @brief The number of suppported DMM knobs configurations
+ *
+ * This is the number of entries in the @ref dmm_knobs table.
+ */
+extern uint32_t dmm_knobs_count __attribute__((weak));
+
+/**
+ * @brief Initialized the DMM Library for Run-Time Resource Management
+ *
+ * @param conf A reference to the DMM knob parameters table
+ * @param count The number of different knobs configurations
+ *
+ * @return 0 on success
+ */
+extern uint32_t dmm_rtm_init(const struct rtm_knobs_t *conf, uint32_t count) __attribute__((weak));
+
+/**
+ * @brief Set the DMM Library knobs parameters
+ *
+ * The DMM Library could be run-time tuned by configuring the value of a set
+ * of knob parameters. This method allow to specify the set of value to be
+ * used to reconfigure the knobs. The @ref conf parameter should reference an
+ * entry of the @ref dmm_knobs array.
+ *
+ * @param conf a pointer to a dmm_knobs entry
+ *
+ * @return 0 on success
+ */
+extern uint32_t dmm_set_knobs(struct rtm_knobs_t *conf) __attribute__((weak));
+
+namespace bbque { namespace rtlib {
+
+bool LibDMM::initialized = false;
+
+LibDMM::LibDMM() {
+	DB(fprintf(stderr, FD("LibDMM ctor\n")));
+}
+
+LibDMM::~LibDMM() {
+	DB(fprintf(stderr, FD("LibDMM dtor\n")));
+}
+
+
+RTLIB_ExitCode_t
+LibDMM::Init(const struct rtm_knobs_t *conf, uint32_t count) {
+
+	if (dmm_knobs == NULL
+			|| dmm_rtm_init == NULL
+			|| dmm_set_knobs == NULL)
+		return RTLIB_ERROR;
+
+	if (dmm_rtm_init(conf, count) != 0)
+		return RTLIB_ERROR;
+
+	initialized = true;
+	return RTLIB_OK;
+
+}
+
+
+RTLIB_ExitCode_t
+LibDMM::SetKnobs(uint32_t index) {
+	DB(fprintf(stderr, FD("Setting DMM Knobs to [%02d]\n"), index));
+
+	if (!initialized)
+		return RTLIB_ERROR;
+
+	if (index >= dmm_knobs_count) {
+		fprintf(stderr, FW("ERROR: Knobs index [%02d] out-of-range\n"),
+				index);
+		return RTLIB_ERROR;
+	}
+
+	if (dmm_set_knobs(&dmm_knobs[index]) != 0) {
+		fprintf(stderr, FW("FAILED: Knobs index [%02d] setting\n"),
+				index);
+		return RTLIB_ERROR;
+	}
+
+	return RTLIB_OK;
+}
+
+} // namespace rtlib
+
+} // namespace bbque
