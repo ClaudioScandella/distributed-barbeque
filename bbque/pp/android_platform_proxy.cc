bb99d4bd2bd67bc1b540d51f3374ebeacf59f7ef
Michele Zanella <michele.zanella@polimi.it>
2018-11-28 15:28:45 +0100

    [PP_Android] Refactor of parameters type due to ProcessManager integration
    Signed-off-by: Michele Zanella <michele.zanella@polimi.it>

--- bbque/pp/android_platform_proxy.cc
+++ bbque/pp/android_platform_proxy.cc
@@ -94,7 +94,7 @@ const char* AndroidPlatformProxy::GetHardwareID(int16_t system_id) const {
 	return "device";
 }
 
-AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::Setup(AppPtr_t papp) {
+AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::Setup(SchedPtr_t papp) {
 	logger->Info("Setup: %s", papp->StrId());
 	return PLATFORM_OK;
 }
@@ -193,18 +193,18 @@ AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::Refresh() {
 	return PLATFORM_OK;
 }
 
-AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::Release(AppPtr_t papp) {
+AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::Release(SchedPtr_t papp) {
 	logger->Info("Release: %s", papp->StrId());
 	return PLATFORM_OK;
 }
 
-AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::ReclaimResources(AppPtr_t papp) {
+AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::ReclaimResources(SchedPtr_t papp) {
 	logger->Info("ReclaimResources: %s", papp->StrId());
 	return PLATFORM_OK;
 }
 
 AndroidPlatformProxy::ExitCode_t AndroidPlatformProxy::MapResources(
-		AppPtr_t papp,
+		SchedPtr_t papp,
 		ResourceAssignmentMapPtr_t pres,bool excl) {
 
 	(void) pres;
