78deaf0e436ba998a9868a856de103cc7ccd07e4
Claudio Scandella <claudio.scandella@outlook.it>
2019-08-24 16:05:02 +0200

    [AgentProxyGRPC] Added function to generalize system id of the wanted resource in GetResourceStatus function

--- plugins/agent_proxy/grpc/agent_proxy.cc
+++ plugins/agent_proxy/grpc/agent_proxy.cc
@@ -167,28 +167,35 @@ uint16_t AgentProxyGRPC::GetSystemId(const std::string & system_path) const {
 			bbque::res::ResourceType::SYSTEM));
 }
 
+ExitCode_t AgentProxyGRPC::GeneralizeSystemID(std::string path, std::string & generalized_path) {
+	res::ResourcePath resPath = res::ResourcePath(path);
 
-std::shared_ptr<AgentClient> AgentProxyGRPC::GetAgentClient(uint16_t remote_system_id) {
-	logger->Debug("GetAgentClient: retrieving a client for sys%d", remote_system_id);
-	if (!platform->ExistSystem(remote_system_id)) {
-		logger->Error("GetAgentClient: sys%d not registered", remote_system_id);
-		return nullptr;
-	}
+	if(resPath.ReplaceID(res::ResourceType::SYSTEM, resPath.GetID(res::ResourceType::SYSTEM), R_ID_NONE) != res::ResourcePath::ExitCode_t::OK)
+		return agent::ExitCode_t::REQUEST_REJECTED;
 
-	auto sys_client = clients.find(remote_system_id);
+	generalized_path = resPath.ToString();
+
+	return agent::ExitCode_t::OK;
+}
+
+std::shared_ptr<AgentClient> AgentProxyGRPC::GetAgentClient(std::string ip) {
+	logger->Debug("GetAgentClient: retrieving a client for ip %s", ip.c_str());
+	// if (!platform->ExistSystem(remote_system_id)) {
+	// 	logger->Error("GetAgentClient: sys%d not registered", remote_system_id);
+	// 	return nullptr;
+	// }
+
+	auto sys_client = clients.find(ip);
 	if(sys_client == clients.end()) {
-		logger->Debug("GetAgentClient: creating a client for sys%d", remote_system_id);
-		std::string server_address_port(platform->GetSystem(remote_system_id).GetNetAddress());
-		server_address_port.append(":" + std::to_string(port_num));
-		logger->Debug("GetAgentClient: allocating a client to connect to --> %s",
-			server_address_port.c_str());
+		logger->Debug("GetAgentClient: creating a client for ip %s", ip.c_str());
 
+		// Al posto di server_address_port bisogna mettere l'ip effettivo
 		std::shared_ptr<AgentClient> client_ptr =
-			std::make_shared<AgentClient>(local_sys_id, remote_system_id, server_address_port);
-		clients.emplace(remote_system_id, client_ptr);
+			std::make_shared<AgentClient>(ip);
+		clients.emplace(ip, client_ptr);
 	}
 	logger->Debug("GetAgentClient: active clients = %d", clients.size());
-	return clients.at(remote_system_id);
+	return clients.at(ip);
 }
 
 ExitCode_t AgentProxyGRPC::Discover(
@@ -228,11 +235,29 @@ ExitCode_t AgentProxyGRPC::Ping(
 	}
 
 ExitCode_t AgentProxyGRPC::GetResourceStatus(
+		int16_t instance_id,
 		std::string const & resource_path,
 		agent::ResourceStatus & status) {
-	std::shared_ptr<AgentClient> client(GetAgentClient(GetSystemId(resource_path)));
+
+	std::string general_path;
+
+	if(GeneralizeSystemID(resource_path, general_path) != agent::ExitCode_t::OK)
+		return agent::ExitCode_t::REQUEST_REJECTED;
+
+	std::map<int, std::string> inst_map = dism.GetInstancesID();
+	std::string ip = inst_map[instance_id];
+	logger->Debug("ip: %s", ip.c_str());
+
+	std::shared_ptr<AgentClient> client(GetAgentClient(ip));
 	if (client)
-		return client->GetResourceStatus(resource_path, status);
+	{
+		logger->Debug("client found");
+		logger->Debug("Calling client->GetResourceStatus(path, status)");
+		logger->Debug("path: %s", general_path.c_str());
+		return client->GetResourceStatus(general_path, status);
+	}
+	logger->Debug("client NOT found");
+
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
 }
 
@@ -243,14 +268,16 @@ ExitCode_t AgentProxyGRPC::GetWorkloadStatus(
 }
 
 ExitCode_t AgentProxyGRPC::GetWorkloadStatus(
-		int remote_system_id,
+		int16_t instance_id,
 		agent::WorkloadStatus & status) {
-	std::shared_ptr<AgentClient> client(GetAgentClient(remote_system_id));
+	std::map<int, std::string> inst_map = dism.GetInstancesID();
+	std::string ip = inst_map[instance_id];
+
+	std::shared_ptr<AgentClient> client(GetAgentClient(ip));
 	if (client)
 		return client->GetWorkloadStatus(status);
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
 }
-
 
 ExitCode_t AgentProxyGRPC::GetChannelStatus(
 		std::string const & path,
@@ -259,9 +286,12 @@ ExitCode_t AgentProxyGRPC::GetChannelStatus(
 }
 
 ExitCode_t AgentProxyGRPC::GetChannelStatus(
-		int remote_system_id,
+		int16_t instance_id,
 		agent::ChannelStatus & status) {
-	std::shared_ptr<AgentClient> client(GetAgentClient(remote_system_id));
+	std::map<int, std::string> inst_map = dism.GetInstancesID();
+	std::string ip = inst_map[instance_id];
+
+	std::shared_ptr<AgentClient> client(GetAgentClient(ip));
 	if (client)
 		return client->GetChannelStatus(status);
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
@@ -275,7 +305,7 @@ ExitCode_t AgentProxyGRPC::SendJoinRequest(std::string const & path) {
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
 }
 
-ExitCode_t AgentProxyGRPC::SendJoinRequest(int remote_system_id) {
+ExitCode_t AgentProxyGRPC::SendJoinRequest(int16_t instance_id) {
 
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
 }
@@ -286,7 +316,7 @@ ExitCode_t AgentProxyGRPC::SendDisjoinRequest(std::string const & path) {
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
 }
 
-ExitCode_t AgentProxyGRPC::SendDisjoinRequest(int remote_system_id) {
+ExitCode_t AgentProxyGRPC::SendDisjoinRequest(int16_t instance_id) {
 
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
 }
@@ -295,7 +325,7 @@ ExitCode_t AgentProxyGRPC::SendDisjoinRequest(int remote_system_id) {
 // ----------- Scheduling / Resource allocation functions ----------
 
 ExitCode_t AgentProxyGRPC::SendScheduleRequest(
-		std::string const & path,
+		int16_t instance_id,
 		agent::ApplicationScheduleRequest const & request) {
 
 	return agent::ExitCode_t::AGENT_UNREACHABLE;
